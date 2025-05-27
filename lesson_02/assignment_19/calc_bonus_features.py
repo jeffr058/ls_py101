5f67d04fd302a8be9c0b13c9cc2d0cec3f64b7ee
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-27 15:34:10 -0700

    start new calc if user inputs Y

---
+++ lesson_02/assignment_19/calc_bonus_features.py
@@ -0,0 +1,51 @@
+def prompt(message):
+    print(f"==> {message}")
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator')
+
+new_calc = 'Y'
+
+while new_calc == 'Y':
+    prompt('Please enter the first number: ')
+    number1 = input()
+
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
+
+    prompt('Please enter the second number: ')
+    number2 = input()
+
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
+
+    prompt('Choose your operation:\n1) Add 2) Subtract 3) Multiply 4) Divide')
+    operation = input()
+
+    while operation not in ['1', '2', '3', '4']:
+        prompt('You must choose 1, 2, 3, or 4')
+        operation = input()
+
+    match operation:
+        case '1':
+            output = int(number1) + int(number2)
+        case '2':
+            output = int(number1) - int(number2)
+        case '3':
+            output = int(number1) * int(number2)
+        case '4':
+            output = int(number1) / int(number2)
+
+    prompt(f'The result is: {output}')
+
+    prompt('Perform another calculation?\nY) Yes N) No')
+    new_calc = input()
