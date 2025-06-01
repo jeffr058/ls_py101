a191f4c247facde33ab886b44ffe52b61d62d8fa
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-05-31 23:19:32 -0700

    add initial attempt to check and clean input

---
+++ lesson_02/assignment_21/loan_calc.py
@@ -0,0 +1,40 @@
+def prompt(message):
+    print(f'==> {message}')
+
+def check_input(input_str):
+    if (input_str == '') or ('-' in input_str):
+        return False
+    elif clean_input(input_str):  # need better way to pass only numbers through
+        return True
+    else:
+        return False
+    
+def clean_input(input_str):
+    symbols = ['$', ',']
+    
+    for symbol in symbols:
+        if symbol in input_str:
+            input_str = input_str.replace(symbol, '')
+    
+    clean_str = input_str
+    
+    return clean_str
+
+def num_type(clean_str):
+    if '.' in clean_str:
+        return float(clean_str)
+    else:
+        return int(clean_str)
+
+prompt('What is the loan amount?')
+loan_amount = input()
+
+while not check_input(loan_amount):
+    prompt('Please enter a valid amount.')
+    loan_amount = input()
+
+# prompt('What is the APR?')
+# apr = input()
+
+# prompt('What is the loan duration?')
+# loan_duration = input()
