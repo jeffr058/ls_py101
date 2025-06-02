fb750c3e7919766a0d056d32bd338b1ab6855e4c
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-06-02 16:11:37 -0700

    refactor to remove clean_input()

--- lesson_02/assignment_21/loan_calc.py
+++ lesson_02/assignment_21/loan_calc.py
@@ -12,17 +12,6 @@ def check_input(input_str):
     else:
         return True
     
-def clean_input(input_str):
-    symbols = ['$', ',']
-    
-    for symbol in symbols:
-        if symbol in input_str:
-            input_str = input_str.replace(symbol, '')
-    
-    clean_str = input_str
-    
-    return clean_str
-    
 def set_num_type(clean_str):
     if '.' in clean_str:
         return float(clean_str)
@@ -30,15 +19,16 @@ def set_num_type(clean_str):
         return int(clean_str)
 
 prompt('What is the loan amount?')
-loan_amount = clean_input(input())
+loan_amount = input().replace('$', '').replace(',', '')
 
 while not check_input(loan_amount):
     prompt('Please enter a valid amount.')
-    loan_amount = clean_input(input())
+    loan_amount = input().replace('$', '').replace(',', '')
 
-loan_amount_clean = set_num_type(loan_amount)
-print(type(loan_amount_clean))
-print(loan_amount_clean)
+# loan_amount_clean = set_num_type(loan_amount)
+
+print(type(loan_amount))
+print(loan_amount)
 
 # prompt('What is the APR?')
 # apr = input()
