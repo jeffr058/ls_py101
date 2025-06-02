285462709fc057b28a510e3356492876125e917a
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-06-02 16:29:07 -0700

    add code for APR

--- lesson_02/assignment_21/loan_calc.py
+++ lesson_02/assignment_21/loan_calc.py
@@ -12,20 +12,26 @@ def check_input(input_str):
     
     return False
 
-prompt('What is the loan amount?')
-loan_amount = input().replace('$', '').replace(',', '')
+# prompt('What is the loan amount?')
+# loan_amount = input().replace('$', '').replace(',', '')
 
-while check_input(loan_amount):
-    prompt('Please enter a valid amount.')
-    loan_amount = input().replace('$', '').replace(',', '')
+# while check_input(loan_amount):
+#     prompt('Please enter a valid amount.')
+#     loan_amount = input().replace('$', '').replace(',', '')
 
-loan_amount_clean = float(loan_amount)
+# loan_amount_clean = float(loan_amount)
 
-print(type(loan_amount_clean))
-print(loan_amount_clean)
+prompt('What is the APR?')
+apr = input()
 
-# prompt('What is the APR?')
-# apr = input()
+while check_input(apr):
+    prompt('Please enter a valid number.')
+    apr = input()
+
+apr_clean = float(apr)
+
+print(type(apr_clean))
+print(apr_clean)
 
 # prompt('What is the loan duration?')
 # loan_duration = input()
