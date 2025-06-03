4d05693311ab766be1ec8b15adc1d945d0817d77
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-06-03 10:41:56 -0700

    transfer messages to json file

--- lesson_02/assignment_21/loan_calc.py
+++ lesson_02/assignment_21/loan_calc.py
@@ -1,3 +1,8 @@
+import json
+
+with open('loan_calc_messages.json', 'r') as file:
+    messages = json.load(file)
+
 def prompt(message):
     print(f'==> {message}')
 
@@ -16,32 +21,34 @@ def calculate_monthly_payment(amount, int_rate, length):
     monthly_payment = amount * (int_rate / (1 - (1 + int_rate) ** (-length)))
     return round(monthly_payment, 2)
 
+prompt(messages['welcome'])
+
 while True:
 
-    prompt('What is the loan amount?')
+    prompt(messages['get_loan_amount'])
     loan_amount = input().replace('$', '').replace(',', '')
 
     while is_input_number(loan_amount):
-        prompt('Please enter a valid amount.')
+        prompt(messages['invalid_input'])
         loan_amount = input().replace('$', '').replace(',', '')
 
     loan_amount_clean = float(loan_amount)
 
-    prompt('What is the APR?')
+    prompt(messages['get_apr'])
     apr = input()
 
     while is_input_number(apr):
-        prompt('Please enter a valid number.')
+        prompt(messages['invalid_input'])
         apr = input()
 
     apr_clean = float(apr) / 100
     apr_monthly = apr_clean / 12
 
-    prompt('What is the loan duration in years?')
+    prompt(messages['get_loan_duration'])
     loan_duration = input()
 
     while is_input_number(loan_duration):
-        prompt('Please enter a valid number.')
+        prompt(messages['invalid_input'])
         loan_duration = input()
 
     loan_duration_clean = float(loan_duration)
@@ -49,10 +56,11 @@ while True:
 
     monthly_payment = calculate_monthly_payment(loan_amount_clean, apr_monthly, loan_duration_months)
 
-    prompt(f'The monthly payment is ${monthly_payment}.')
+    prompt(messages['monthly_payment']+ f'{monthly_payment}.')
 
-    prompt('Perform new calculation?\nY) Yes  N) No')
+    prompt(messages['new_calc'])
     new_calc = input()
 
     if new_calc and (new_calc[0].casefold() != 'y'):
+        prompt(messages['goodbye'])
         break
