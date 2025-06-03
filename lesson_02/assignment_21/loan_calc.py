163ad93b53bd82be12812ed2b2b72a54f160f67a
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-06-03 10:07:44 -0700

    add new calculation option

--- lesson_02/assignment_21/loan_calc.py
+++ lesson_02/assignment_21/loan_calc.py
@@ -16,35 +16,43 @@ def calculate_monthly_payment(amount, int_rate, length):
     monthly_payment = amount * (int_rate / (1 - (1 + int_rate) ** (-length)))
     return round(monthly_payment, 2)
 
-prompt('What is the loan amount?')
-loan_amount = input().replace('$', '').replace(',', '')
+while True:
 
-while is_input_number(loan_amount):
-    prompt('Please enter a valid amount.')
+    prompt('What is the loan amount?')
     loan_amount = input().replace('$', '').replace(',', '')
 
-loan_amount_clean = float(loan_amount)
+    while is_input_number(loan_amount):
+        prompt('Please enter a valid amount.')
+        loan_amount = input().replace('$', '').replace(',', '')
 
-prompt('What is the APR?')
-apr = input()
+    loan_amount_clean = float(loan_amount)
 
-while is_input_number(apr):
-    prompt('Please enter a valid number.')
+    prompt('What is the APR?')
     apr = input()
 
-apr_clean = float(apr) / 100
-apr_monthly = apr_clean / 12
+    while is_input_number(apr):
+        prompt('Please enter a valid number.')
+        apr = input()
 
-prompt('What is the loan duration in years?')
-loan_duration = input()
+    apr_clean = float(apr) / 100
+    apr_monthly = apr_clean / 12
 
-while is_input_number(loan_duration):
-    prompt('Please enter a valid number.')
+    prompt('What is the loan duration in years?')
     loan_duration = input()
 
-loan_duration_clean = float(loan_duration)
-loan_duration_months = loan_duration_clean * 12
+    while is_input_number(loan_duration):
+        prompt('Please enter a valid number.')
+        loan_duration = input()
 
-monthly_payment = calculate_monthly_payment(loan_amount_clean, apr_monthly, loan_duration_months)
+    loan_duration_clean = float(loan_duration)
+    loan_duration_months = loan_duration_clean * 12
 
-print(monthly_payment)
+    monthly_payment = calculate_monthly_payment(loan_amount_clean, apr_monthly, loan_duration_months)
+
+    prompt(f'The monthly payment is ${monthly_payment}.')
+
+    prompt('Perform new calculation?\nY) Yes  N) No')
+    new_calc = input()
+
+    if new_calc and (new_calc[0].casefold() != 'y'):
+        break
