bb1aae7aec6f96fb4ef9b0ed20b2865b7f7b25b9
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-06-03 11:57:14 -0700

    get user to confirm inputs

--- lesson_02/assignment_21/loan_calc.py
+++ lesson_02/assignment_21/loan_calc.py
@@ -17,6 +17,17 @@ def is_input_number(input_str):
     
     return False
 
+def is_input_correct(input_type, display_input):
+    prompt(messages[input_type] + display_input)
+    prompt(messages['is_correct'])
+
+    user_confirm = input()
+
+    if user_confirm and (user_confirm[0].casefold() != 'y'):
+        return False
+    else:
+        return True
+
 def calculate_monthly_payment(amount, int_rate, length):
     monthly_payment = amount * (int_rate / (1 - (1 + int_rate) ** (-length)))
     return round(monthly_payment, 2)
@@ -25,31 +36,49 @@ prompt(messages['welcome'])
 
 while True:
 
-    prompt(messages['get_loan_amount'])
-    loan_amount = input().replace('$', '').replace(',', '')
-
-    while is_input_number(loan_amount):
-        prompt(messages['invalid_input'])
+    while True:
+        prompt(messages['get_loan_amount'])
         loan_amount = input().replace('$', '').replace(',', '')
 
-    loan_amount_clean = float(loan_amount)
+        while is_input_number(loan_amount):
+            prompt(messages['invalid_input'])
+            loan_amount = input().replace('$', '').replace(',', '')
+        
+        loan_amount_clean = float(loan_amount)
 
-    prompt(messages['get_apr'])
-    apr = input()
+        display_loan_amount = f'{loan_amount_clean:,.2f}.'
 
-    while is_input_number(apr):
-        prompt(messages['invalid_input'])
+        if is_input_correct('confirm_loan_amount', display_loan_amount):
+            break
+
+    while True:
+        prompt(messages['get_apr'])
         apr = input()
+
+        while is_input_number(apr):
+            prompt(messages['invalid_input'])
+            apr = input()
+
+        display_apr = f'{apr}%.'
+        
+        if is_input_correct('confirm_apr', display_apr):
+            break
 
     apr_clean = float(apr) / 100
     apr_monthly = apr_clean / 12
 
-    prompt(messages['get_loan_duration'])
-    loan_duration = input()
-
-    while is_input_number(loan_duration):
-        prompt(messages['invalid_input'])
+    while True:
+        prompt(messages['get_loan_duration'])
         loan_duration = input()
+
+        while is_input_number(loan_duration):
+            prompt(messages['invalid_input'])
+            loan_duration = input()
+
+        display_loan_duration = f'{loan_duration} years.'
+
+        if is_input_correct('confirm_loan_duration', display_loan_duration):
+            break
 
     loan_duration_clean = float(loan_duration)
     loan_duration_months = loan_duration_clean * 12
